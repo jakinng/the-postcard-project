9d23844de9d2e4b6bb559deaa03a02108766d473
xueenwu <jakin.ng03@gmail.com>
2022-06-24 12:56:21 -0700

    Users can upload a photo from the camera

--- app/src/main/java/com/example/thepostcardproject/fragments/CreateFragment.java
+++ app/src/main/java/com/example/thepostcardproject/fragments/CreateFragment.java
@@ -1,60 +1,71 @@
 package com.example.thepostcardproject.fragments;
 
+import static android.app.Activity.RESULT_OK;
+import static androidx.core.content.PermissionChecker.checkSelfPermission;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.content.FileProvider;
+import androidx.core.content.PermissionChecker;
 import androidx.fragment.app.Fragment;
 
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.provider.Settings;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.example.thepostcardproject.R;
+import com.google.android.material.snackbar.Snackbar;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link CreateFragment#newInstance} factory method to
+ * Use the {@link CreateFragment# newInstance} factory method to
  * create an instance of this fragment.
  */
 public class CreateFragment extends Fragment {
+    private final static String TAG = "CreateFragment";
+    public final static int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 1034;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    EditText etMessage;
+    EditText etSendTo;
+    ImageButton ibSendPostcard;
+    ImageView ivCoverPhoto;
+    ImageView ivAddButton;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    ActivityResultLauncher<String> requestPermissionLauncher;
+    String photoFilePath;
 
     public CreateFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment CreateFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static CreateFragment newInstance(String param1, String param2) {
-        CreateFragment fragment = new CreateFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+        addRequestPermissionLauncher();
     }
 
     @Override
@@ -62,5 +73,132 @@ public class CreateFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_create, container, false);
+    }
+
+    /**
+     * Called shortly after onCreateView
+     * @param view The view containing the various visual components
+     * @param savedInstanceState
+     */
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        setupViews(view);
+        setupTakePhoto();
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                Bitmap capturedImage = BitmapFactory.decodeFile(photoFilePath);
+                ivCoverPhoto.setImageBitmap(capturedImage);
+            } else {
+                Snackbar.make(ivAddButton, "Picture wasn't taken!", Snackbar.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    /**
+     * Attaches the views to the corresponding variables
+     * @param view The encapsulating view
+     */
+    private void setupViews(View view) {
+        etMessage = view.findViewById(R.id.et_message);
+        etSendTo = view.findViewById(R.id.et_sendto);
+        ibSendPostcard = view.findViewById(R.id.ib_send_postcard);
+        ivCoverPhoto = view.findViewById(R.id.iv_cover_photo);
+        ivAddButton = view.findViewById(R.id.iv_add_button);
+    }
+
+    // ***************************************************
+    // **  HELPER METHODS FOR DEALING WITH THE CAMERA   **
+    // ***************************************************
+    /**
+     * Defines the request permission launcher
+     * Upon success, calls launchCamera
+     * Upon failure, displays a Toast
+     */
+    private void addRequestPermissionLauncher() {
+        requestPermissionLauncher = registerForActivityResult(
+                new ActivityResultContracts.RequestPermission(),
+                new ActivityResultCallback<Boolean>() {
+                    @Override
+                    public void onActivityResult(Boolean result) {
+                        if (result) {
+                            // PERMISSION GRANTED
+                            Snackbar.make(ivAddButton, "Camera access granted!", Snackbar.LENGTH_SHORT).show();
+                            launchCamera();
+                        } else {
+                            // PERMISSION NOT GRANTED
+                            Snackbar.make(ivAddButton, "Camera access denied! Enable the in-app camera in Settings.", Snackbar.LENGTH_SHORT)
+                                    .setAction("Settings", new View.OnClickListener() {
+                                        @Override
+                                        public void onClick(View v) {
+                                            startActivity(new Intent(Settings.ACTION_SETTINGS));
+                                        }
+                                    })
+                                    .show();
+                        }
+                    }
+                });
+    }
+
+    /**
+     * Launch the in-app camera
+     */
+    private void launchCamera() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getContext().getPackageManager()) != null) {
+            // Create a File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+                Uri photoURI = FileProvider.getUriForFile(getContext(), "com.postcard.fileprovider", photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+            } catch (IOException exception) {
+                Snackbar.make(ivAddButton, "Sorry, an error occurred while taking the photo.", Snackbar.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    /**
+     * When the "add photo" button is clicked, checks for permission and launches the camera
+     */
+    private void setupTakePhoto() {
+        ivCoverPhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (checkSelfPermission(getContext(), Manifest.permission.CAMERA)
+                        != PermissionChecker.PERMISSION_GRANTED) {
+                    requestPermissionLauncher.launch(Manifest.permission.CAMERA);
+                } else {
+                    launchCamera();
+                }
+            }
+        });
+    }
+
+    /**
+     * Creates an image file with a unique name
+     * @return Returns a new image stored in a unique file path
+     * @throws IOException
+     */
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "postcard_" + timeStamp + "_";
+        File storageDir = getContext().getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        photoFilePath = image.getAbsolutePath();
+        return image;
     }
 }
